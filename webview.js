1991476f4d35dcb5f4b6397ddc6ebc9f5a5f844d
Dave Reid <dave@davereid.net>
2017-11-20 11:36:29 -0600

    Fixed use let instead of const.

--- webview.js
+++ webview.js
@@ -1,26 +1,27 @@
 "use strict";
 
-const path = require('path');
-
 module.exports = Franz => {
   const getMessages = function getMessages() {
-    const notifications = 0;
-    const newUnreads = 0;
+    let directMessages = 0;
+    let indirectMessages = 0;
 
-    const notificationElement = document.getElementsByClassName('yj-notifications-indicator-count');
-    const newMessagesElement = document.getElementsByClassName("[yj-thread-list--new-message-text");
+    const unreadElement = document.getElementsByClassName("unread-count");
+    const notificationElement = document.getElementsByClassName("yj-notifications-indicator-count");
+    const newMessagesElement = document.getElementsByClassName("yj-thread-list--new-message-text");
 
     if (notificationElement.length) {
-      notifications = parseInt(notificationElement[0].innerHTML, 10);
+      directMessages = parseInt(notificationElement[0].innerHTML, 10);
+    }
+
+    if (unreadElement.length) {
+      //directMessages = parseInt(unreadElement[0].innerHTML, 10);
     }
 
     if (newMessagesElement.length) {
-      newUnreads = parseInt(newMessagesElement[0].innerHTML.match(/\d+/)[0], 10);
+      indirectMessages = parseInt(newMessagesElement[0].innerHTML.match(/\d+/)[0], 10);
     }
 
-    // First parameter is direct notifications, second parameter is indirect
-    // notifications.
-    Franz.setBadge(notifications, newUnreads);
+    Franz.setBadge(directMessages, indirectMessages);
   };
 
   Franz.loop(getMessages);
