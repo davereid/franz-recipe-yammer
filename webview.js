5210f972f35b1a944433d8813caf24d171469437
Dave Reid <dave@davereid.net>
2017-11-20 11:22:10 -0600

    Add support for reading the new messages indicator.

--- webview.js
+++ webview.js
@@ -5,13 +5,22 @@ const path = require('path');
 module.exports = Franz => {
   const getMessages = function getMessages() {
     const notifications = 0;
-    const notificationElement = document.querySelector("span[data-qaid=notifications_unread_count_label]");
+    const newUnreads = 0;
 
-    if (notificationElement) {
-      notifications = parseInt(notificationElement.innerHTML, 10);
+    const notificationElement = document.getElementsByClassName('yj-notifications-indicator-count');
+    const newMessagesElement = document.getElementsByClassName("[yj-thread-list--new-message-text");
+
+    if (notificationElement.length) {
+      notifications = parseInt(notificationElement[0].innerHTML, 10);
     }
 
-    Franz.setBadge(notifications);
+    if (newMessagesElement.length) {
+      newUnreads = parseInt(newMessagesElement[0].innerHTML.match(/\d+/)[0], 10);
+    }
+
+    // First parameter is direct notifications, second parameter is indirect
+    // notifications.
+    Franz.setBadge(notifications, newUnreads);
   };
 
   Franz.loop(getMessages);
